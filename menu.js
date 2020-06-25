1cfd0aca39a05c94091f5c565e455c654f1d6b17
nathan-vanb3 <natha1@umbc.edu>
2020-06-25 15:29:21 -0400

    Added javascript for main menu.

---
+++ menu.js
@@ -0,0 +1,22 @@
+function dropdownSelect() {
+    var selection = document.getElementById("dropdown").value;
+
+    if(selection.equals("callMort")) {
+        location.href = "https://health.maryland.gov/bom/Pages/home.aspx";
+    }
+
+/*
+    else if(selection.equals("fileded")){
+        var patient = prompt("Please input the patient's name.");
+
+
+
+        var causeOfDeath = prompt("Please input the cause of death.");
+
+
+    }
+*/
+
+    
+}
+
