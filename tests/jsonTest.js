e5ad78d6e24d8d636cc43791711ebd6d97716535
nathan-vanb3 <natha1@umbc.edu>
2020-06-26 11:41:31 -0400

    testing JSON implementation.

---
+++ tests/jsonTest.js
@@ -0,0 +1,40 @@
+function person(name, age) {
+    this.name = name;
+    this. age = age;
+}
+
+var intialArray = [new person("george", 40), new person("nick", 25)];
+
+function initializeData() {
+    if(localStorage.getItem("people") == null) {
+        var initialArray = [new person("george", 40), new person("nick", 25)];
+        localStorage.setItem("people", JSON.stringify(initialArray));
+    }
+
+    else{}
+}
+
+function displayData() {
+    var finalArray = JSON.parse(localStorage.getItem("people"));
+    console.log(JSON.stringify(finalArray));
+}
+
+function displayName() {
+    var finalArray = JSON.parse(localStorage.getItem("people"));
+    console.log(finalArray[0].name);
+}
+
+function displayAge() {
+    var finalArray = JSON.parse(localStorage.getItem("people"));
+    console.log(finalArray[0].age);
+}
+
+function addPerson() {
+    var name = document.getElementById("name").value;
+    var age = parseInt(document.getElementById("age").value);
+
+    var finalArray = JSON.parse(localStorage.getItem("people"));
+    finalArray.push(new person(name, age));
+
+    localStorage.setItem("people", JSON.stringify(finalArray));
+}
